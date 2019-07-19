569e2dbf63791c223bf1516b6c7ca654205b8a1d
shekhar_kony <shekhar.suman@kony.com>
2019-07-19 13:47:32 +0530

    dispense logic changes

--- src/app/services/dispense.service.ts
+++ src/app/services/dispense.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { OrderBeverage } from '../models/OrderBeverage';
+import { Order } from '../models/Order';
 
 @Injectable({
   providedIn: 'root'
@@ -7,41 +7,49 @@ import { OrderBeverage } from '../models/OrderBeverage';
 export class DispenseService {
 
 
-  private activeOrders: OrderBeverage[] = [{
-    orderBeverageId: 1,
+  private activeOrders: Order[] = [{
     orderId: 1,
-    beverage: {
-      beverageId: 2,
-      beverageName: "Hogarden",
-      beverageType: "B"
-    }
-  },
-  {
-    orderBeverageId: 2,
-    orderId: 1,
-    beverage: {
+    beverages: [{
       beverageId: 2,
       beverageName: "Hogarden",
       beverageType: "B",
-    }
-  },
-  {
-    orderBeverageId: 3,
-    orderId: 1,
-    beverage: {
+      quantity: 3
+    }, {
       beverageId: 1,
       beverageName: "Kingfisher",
       beverageType: "B",
-    }
+      quantity: 2
+    }]
   }];
   constructor() { }
 
-  getActiveOrders(): OrderBeverage[] {
+  getActiveOrders(): Order[] {
     return [...this.activeOrders];
   }
 
-  dispenseOrder(orderBeverageId: number) {
-    console.log("Order dispensed!", orderBeverageId);
+
+
+  dispenseOrder(orderId: number, beverageId: number) {
+    let dispenseSatus = 'S';
+    console.log('Order dispensed!', orderId, beverageId);
+    if (dispenseSatus === 'S') {
+      //removing from active Orders array
+
+      for (let i = 0; i < this.activeOrders.length; i++) {
+        if (this.activeOrders[i].orderId === orderId) {
+          for (let j = 0; j < this.activeOrders[i].beverages.length; j++) {
+            if (+this.activeOrders[i].beverages[j].beverageId === beverageId) {
+              if (this.activeOrders[i].beverages[j].quantity === 1) {
+                this.activeOrders[i].beverages.splice(j, 1);
+              } else {
+                this.activeOrders[i].beverages[j].quantity--;
+              }
+            }
+          }
+        }
+      }
+
+    }
   }
 
 

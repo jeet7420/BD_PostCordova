c213eea4832c0c9aaf91be80caf61818d226e05b
shekhar_kony <shekhar.suman@kony.com>
2019-07-22 15:28:44 +0530

    implemented settings page

--- src/app/pages/settings/settings.page.ts
+++ src/app/pages/settings/settings.page.ts
@@ -8,7 +8,9 @@ import { Component, OnInit } from '@angular/core';
 export class SettingsPage implements OnInit {
 
   constructor() { }
-
+  allNotif: boolean;
+  newPlaces: boolean;
+  newOffers: boolean;
   ngOnInit() {
   }
 
